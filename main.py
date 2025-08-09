8be4f17583316bafd39d2015f9d1efa733f24d24
Abolfazl Tashakori <abolfazltashakori809@gmail.com>
2025-08-09 14:44:19 +0330

    1.4 Admin Menu Added!

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ payment_handler = PaymentHandler(bot)
 @bot.on_message(filters.command("start"))
 async def start_handler(client: Client, message: Message):
     user = message.from_user
+    user_id = message.from_user.id
+    admin_id = 5381391685
     db = VpnDatabase()
     db.create_user_if_not_exists(
         telegram_id=user.id,  # تغییر از user_id به telegram_id
@@ -34,17 +36,29 @@ async def start_handler(client: Client, message: Message):
         join_date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # اضافه کردن تاریخ عضویت
     )
     db.close()
-
-    keyboard = [
-        [InlineKeyboardButton("دریافت اکانت تست رایگان", callback_data="test_vpn_menu")],
-        [InlineKeyboardButton("خرید سرویس جدید", callback_data="buy_new_service_menu"),
-         InlineKeyboardButton("سرویس های من", callback_data="my_service_menu")],
-        [InlineKeyboardButton("مدیریت کیف پول", callback_data="money_managment"),
-         InlineKeyboardButton("تعرفه بسته ها", callback_data="price_info")],
-        [InlineKeyboardButton("آموزش اتصال", callback_data="connection_info")],
-        [InlineKeyboardButton("ارتباط با پشتیبانی", callback_data="support"),
-         InlineKeyboardButton("مشخصات کاربری", callback_data="user_details")],
-    ]
+    if user_id == admin_id:
+        keyboard = [
+            [InlineKeyboardButton("دریافت اکانت تست رایگان", callback_data="test_vpn_menu")],
+            [InlineKeyboardButton("خرید سرویس جدید", callback_data="buy_new_service_menu"),
+             InlineKeyboardButton("سرویس های من", callback_data="my_service_menu")],
+            [InlineKeyboardButton("مدیریت کیف پول", callback_data="money_managment"),
+             InlineKeyboardButton("تعرفه بسته ها", callback_data="price_info")],
+            [InlineKeyboardButton("آموزش اتصال", callback_data="connection_info")],
+            [InlineKeyboardButton("ارتباط با پشتیبانی", callback_data="support"),
+             InlineKeyboardButton("مشخصات کاربری", callback_data="user_details")],
+            [InlineKeyboardButton("بخش ادمین",callback_data="admin_menu")],
+        ]
+    else:
+        keyboard = [
+            [InlineKeyboardButton("دریافت اکانت تست رایگان", callback_data="test_vpn_menu")],
+            [InlineKeyboardButton("خرید سرویس جدید", callback_data="buy_new_service_menu"),
+             InlineKeyboardButton("سرویس های من", callback_data="my_service_menu")],
+            [InlineKeyboardButton("مدیریت کیف پول", callback_data="money_managment"),
+             InlineKeyboardButton("تعرفه بسته ها", callback_data="price_info")],
+            [InlineKeyboardButton("آموزش اتصال", callback_data="connection_info")],
+            [InlineKeyboardButton("ارتباط با پشتیبانی", callback_data="support"),
+             InlineKeyboardButton("مشخصات کاربری", callback_data="user_details")],
+        ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     text = f"""
     🌟 سلام {message.from_user.first_name} عزیز!
